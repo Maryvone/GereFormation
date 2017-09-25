0ba6f1f6674a01f393c22c540c83d1a5fe231efc
Maryvone <marioncueff@gmail.com>
2017-09-25 16:29:55 +0200

    DBconnect added

--- src/ECF.java
+++ src/ECF.java
@@ -5,4 +5,46 @@ public class ECF {
     private Stagiaire idStagiaire;
     private Module idModule;
 
+    public ECF(int note, String nom, Stagiaire idStagiaire, Module idModule) {
+        this.note = note;
+        this.nom = nom;
+        this.idStagiaire = idStagiaire;
+        this.idModule = idModule;
+    }
+
+    public int getNote() {
+        return note;
+    }
+
+    public void setNote(int note) {
+        this.note = note;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public Stagiaire getIdStagiaire() {
+        return idStagiaire;
+    }
+
+    public void setIdStagiaire(Stagiaire idStagiaire) {
+        this.idStagiaire = idStagiaire;
+    }
+
+    public Module getIdModule() {
+        return idModule;
+    }
+
+    public void setIdModule(Module idModule) {
+        this.idModule = idModule;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
